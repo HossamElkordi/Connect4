a9033f2437ca2181d4048f1d6d0f86c94f3ab777
HossamElkordi <hossam_elkordi@yahoo.com>
2021-12-14 12:56:14 +0200

    Fixing tree visualization bugs

--- tree.py
+++ tree.py
@@ -4,10 +4,11 @@ from networkx.drawing.nx_pydot import graphviz_layout
 
 
 class TreeNode:
-    def __init__(self, val, children, branch):
+    def __init__(self, val, children, branch, col):
         self.val = val
         self.child = children
         self.br = branch
+        self.col = col
 
     def value(self):
         return self.val
@@ -39,12 +40,43 @@ def traverse(child, g, node_labels):
     for n in child[1].children():
         g.add_node(child[0] * 10 + i, value=n.value())
         node_labels[child[0] * 10 + i] = n.value()
-        g.add_weighted_edges_from([(child[0], child[0] * 10 + i, n.branch())], weight='col')
+        g.add_weighted_edges_from([(child[0], child[0] * 10 + i, n.col)], weight='col')
         traverse((child[0] * 10 + i, n), g, node_labels)
         i += 1
 
 
+def check_row(board, window, col_pos, i, val):
+    if not val and \
+            (col_pos[window[1]] >= i or col_pos[window[1] + 1] >= i or col_pos[window[1] + 2] >= i or col_pos[window[1] + 3] >= i):
+        return 0
+    if board[i, window[1]] == val and board[i, window[1] + 1] == val and board[i, window[1] + 2] == val and board[i, window[1] + 3] == val:
+        return 1
+    return 0
+
+
+def check_col(board, window, col_pos, j, val):
+    if col_pos[window[1]] < window[0] - 4 and \
+            board[window[1], j] == val and board[window[1] + 1, j] == val and board[window[1] + 2, j] == val and board[window[1] + 3, j] == val:
+        return 1
+    return 0
+
+
+def check_diagonals(board, window, col_pos, val):
+    pts = 0
+    if window[0, 0] == val and window[1, 1] == val and window[2, 2] == val and window[3, 3] == val:
+        pts += 1
+    if window[0, 3] == val and window[1, 2] == val and window[2, 1] == val and window[3, 0] == val:
+        pts += 1
+    return pts
+
+
+def calc_pts(board, val, all_cols=False):
+    pts = 0
+    for i in range(5, 2, -1):
+        for j in range(4):
+            pass
+
+
 if __name__ == '__main__':
-    root = TreeNode(1, [TreeNode(2, [TreeNode(4, None, -1), TreeNode(4, None, -1)], -1), TreeNode(2, [TreeNode(4, None, -1), TreeNode(4, None, -1)], -1), TreeNode(3, [TreeNode(4, [TreeNode(4, [TreeNode(4, None, -1), TreeNode(4, None, -1)], -1), TreeNode(4, None, -1)], -1)], -1), TreeNode(4, [TreeNode(4, None, -1), TreeNode(4, None, -1)], -1)], -1)
-    root1 = TreeNode(7, [root, root, root, root], -1)
-    plot_tree(root1)
+    print([i for i in range(3)])
+
